7fabba911b9259bbcdfd6c45cfed11914d0cb443
dimpulsha <dimpulsha@gmail.com>
2022-09-17 00:18:11 +0300

    create logger level

--- src/cli/generate-command.ts
+++ src/cli/generate-command.ts
@@ -2,11 +2,14 @@ import got from 'got';
 import OfferGenerator from '../common/offer-generator/offer-generator.js';
 import { MockOffer } from '../types/mock-data.type.js';
 import { CliCommandInterface } from './cli-command.interface.js';
-import  TSVWriter  from '../common/file-writer/file-writer.js';
+import TSVWriter from '../common/file-writer/file-writer.js';
+import { LoggerService } from '../common/logger/logger.js';
 
 export default class GenerateCommand implements CliCommandInterface {
   public readonly name = '--generate';
   private initialData!: MockOffer;
+
+  private logger = new LoggerService;
 
   public async execute(...parameters:string[]): Promise<void> {
     const [count, filepath, url] = parameters;
@@ -15,7 +18,8 @@ export default class GenerateCommand implements CliCommandInterface {
     try {
       this.initialData = await got.get(url).json();
     } catch {
-      return console.log(`Can't fetch data from ${url}.`);
+      // return console.log(`Can't fetch data from ${url}.`);
+      this.logger.error(`Can't fetch data from ${url}.`);
     }
 
     const offerGeneratorString = new OfferGenerator(this.initialData);
@@ -25,7 +29,8 @@ export default class GenerateCommand implements CliCommandInterface {
       await outputTSVStream.write(offerGeneratorString.generate());
     }
 
-    console.log(`File ${filepath} was created!`);
+    // console.log(`File ${filepath} was created!`);
+    this.logger.info(`File ${filepath} was created!`);
   }
 }
 
---
+++ src/common/logger/logger.ts
@@ -0,0 +1,30 @@
+import pino, {Logger} from 'pino';
+import { LoggerInterface } from './logger.interface.js';
+
+export  class LoggerService implements LoggerInterface {
+  private logger!: Logger;
+
+  constructor() {
+    this.logger = pino({
+      level: 'debug'
+    });
+  }
+
+  public debug(message: string, ...args: unknown[]): void {
+    this.logger.debug(message, ...args);
+  }
+
+  public info(message: string, ...args: unknown[]): void {
+    this.logger.info(message, ...args);
+  }
+
+  public warning(message: string, ...args: unknown[]): void {
+    this.logger.warn(message, ...args);
+  }
+
+  public error(message: string, ...args: unknown[]): void {
+    this.logger.error(message, ...args);
+  }
+
+}
+
