8c9018ec9a65d65256c54a7ce56865625013b023
dimpulsha <dimpulsha@gmail.com>
2022-09-18 18:24:39 +0300

    remove logger from CLI

--- src/cli/import-command.ts
+++ src/cli/import-command.ts
@@ -3,11 +3,9 @@ import { CliCommandInterface } from './cli-command.interface.js';
 import { TSVFileReader } from '../common/file-reader/tsv-file-reader.js';
 import { createOffer } from '../utils/createOffer.js';
 import { getErrorMessage } from '../utils/common-utils.js';
-import { LoggerService } from '../common/logger/logger.js';
 
 export default class VersionCommand implements CliCommandInterface{
   public readonly name = '--import';
-  private logger = new LoggerService;
 
   private onLine(rowOffer: string) {
     const offer = createOffer(rowOffer);
@@ -16,7 +14,6 @@ export default class VersionCommand implements CliCommandInterface{
 
   private onComplete(count: number) {
     console.log(chalk.green(`${count} rows imported.`));
-    // this.logger.info(`${count} rows imported.`);
   }
 
   public async execute(filename: string): Promise<void> {
@@ -27,8 +24,7 @@ export default class VersionCommand implements CliCommandInterface{
     try {
       await importReader.read();
     } catch(err) {
-      // console.log(chalk.hex('#FFA500')(`Can't read the file: ${getErrorMessage(err)}`));
-      this.logger.error(`${getErrorMessage(err)}`);
+      console.log(chalk.hex('#FFA500')(`Can't read the file: ${getErrorMessage(err)}`));
     }
   }
 
