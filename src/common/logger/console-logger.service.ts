29bc42f8936618575e5d28611f9fdbeac7573773
dimpulsha <dimpulsha@gmail.com>
2022-10-03 20:23:57 +0300

    simplify CLI logger

--- src/common/logger/console-logger.service.ts
+++ src/common/logger/console-logger.service.ts
@@ -1,29 +1,21 @@
-import pino, { Logger } from 'pino';
 import { LoggerInterface } from './logger.interface.js';
 
 export class ConsoleLoggerService implements LoggerInterface {
-  private logger!: Logger;
-
-  constructor() {
-    this.logger = pino({
-      level: 'debug'
-    });
-  }
 
   public debug(message: string, ...args: unknown[]): void {
-    this.logger.debug(message, ...args);
+    console.debug(message, ...args);
   }
 
   public info(message: string, ...args: unknown[]): void {
-    this.logger.info(message, ...args);
+    console.info(message, ...args);
   }
 
   public warning(message: string, ...args: unknown[]): void {
-    this.logger.warn(message, ...args);
+    console.warn(message, ...args);
   }
 
   public error(message: string, ...args: unknown[]): void {
-    this.logger.error(message, ...args);
+    console.error(message, ...args);
   }
 
 }
